ef902632555332a97f26f0646dac24cbe58b6362
LiyabonaNgece16 <220289824@mycput.ac.za>
2024-06-10 11:02:41 +0200

    Add files via upload

---
+++ AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function AboutPage() {
+  return (
+    <div className="container">
+      <h1>More About me</h1>
+      <p>I am a 22-year old female who is from a small town called Fort Beaufort
+        I am a third year IT student at the Cape Peninsula University Of Technology.
+      </p>
+    </div>
+  );
+}
+
+export default AboutPage;
---
+++ HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div className="container">
+      <h1>Welcome to my website</h1>
+      <p>I am Liyabona Ngece, this website tells you about who I am, where I am 
+        from as well as gives you all my socials where you can find me.
+      </p>
+    </div>
+  );
+}
+
+export default HomePage;
